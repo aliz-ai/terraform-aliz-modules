11082f023bc3a1dab299ef3bc4bbb1307579c1dd
Muhammad Panji <muhammad.panji@aliz.ai>
2021-09-16 10:34:24 +0700

    Update example test stack locals, remove unused variable

--- cloudsql-export/examples/test-stack/locals.tf
+++ cloudsql-export/examples/test-stack/locals.tf
@@ -2,7 +2,7 @@ locals {
   instance_prefix        = "hellomysql"
   instance_name          = "${local.instance_prefix}-${random_id.suffix.hex}"
 
-  project_id             = "aip-aliz-prod-mp-20210909"
+  project_id             = "aip-aliz-prod-mp-20210916"
   region                 = "europe-west1"
   
   instance_tier          = "db-f1-micro"
--- cloudsql-export/examples/test-stack/variables.tf
+++ cloudsql-export/examples/test-stack/variables.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-  description = "The ID of the project in which resources will be provisioned."
-  type        = string
-}
-
 variable "db_name" {
   description = "The name of the SQL Database instance"
   default     = "example-mysql-public"
