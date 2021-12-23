76c4abc013e2869dfabe374a1ebfc818bfa49930
Marcell Borsos <marcell.borsos@yahoo.com>
2021-12-23 09:31:55 +0100

    changed workdir in example

--- simple-tf-pipeline/example/main.tf
+++ simple-tf-pipeline/example/main.tf
@@ -3,5 +3,5 @@ module "tf_pipeline" {
   project           = var.project
   github_owner      = "aliz-ai"
   github_name       = "terraform-aliz-modules"
-  working_directory = "simple-tf-plan-and-apply/example/target"
+  working_directory = "simple-tf-pipeline/example/target"
 }
