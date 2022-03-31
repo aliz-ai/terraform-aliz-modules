5e626152388a91f4ddab0839b7441dc673ed8f92
Barnabas Hegedus <hbarnabas@outlook.com>
2022-03-31 17:22:34 +0200

    remove "unexpected" project argument

--- terraform-drift-check-http-notifier/main.tf
+++ terraform-drift-check-http-notifier/main.tf
@@ -27,7 +27,6 @@ resource "google_project_iam_member" "drift_check_sa_roles" {
 }
 
 resource "google_storage_bucket_iam_member" "tfstate_access" {
-  project = var.project
   bucket  = var.tfstate_bucket
   role    = "roles/storage.objectAdmin"
   member  = "serviceAccount:${google_service_account.drift_check_sa.email}"
